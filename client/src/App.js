cf8f35c5d5cfd0bd185e03829033f48d53941090
vakul <abhisheksaxena714.vs@gmail.com>
2022-05-23 22:20:34 +0530

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,28 @@
 
 import './App.css';
 import {create as ipfsHttpClient} from 'ipfs-http-client';
+import React,{useState} from 'react';
 
 const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0');
 
 function App() {
+  let[start,setStart]=useState(false);
   return (
-    <div className="App">
-     <label>
+    <div className="App" style={{marginTop:25}}>
+      <button onClick={()=>{
+        setStart(true)
+      }}>IPFS</button>
+      <button onClick={()=>{
+        setStart(true)
+      }}>FILECOIN</button>
+      <button onClick={()=>{
+        setStart(true)}}>ARWEAVE</button>
+      <button onClick={()=>{
+        setStart(true)}}>NFT STORAGE</button>
+      {
+        start?
+      <div>
+      <label>
        <p>Upload Image</p>
        <input placeholder='Upload' type='file'
        onChange={async(e)=>{
@@ -17,9 +32,14 @@ function App() {
          const add = await client.add(file);
          const url=`https://ipfs.infura.io/ipfs/${add.path}`;
          console.log(url);
+         setStart(false)
        }}/>
      </label>
-    </div>
+     </div>:''
+}
+      </div>
+  
+  
   );
 }
 
