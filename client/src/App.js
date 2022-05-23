459ccbd2640cb02d2e8c091c830db188fabb2941
vakul <abhisheksaxena714.vs@gmail.com>
2022-05-23 21:07:10 +0530

    hash received

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,21 @@
 import './App.css';
 import {create as ipfsHttpClient} from 'ipfs-http-client';
 
-const client = ipfsHttpClient('');
+const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0');
+
 function App() {
   return (
     <div className="App">
      <label>
        <p>Upload Image</p>
        <input placeholder='Upload' type='file'
-       onChange={(e)=>{
+       onChange={async(e)=>{
          const file = e.target.files[0]
          console.log(file);
+         console.log(client);
+         const add = await client.add(file);
+         const url=`https://ipfs.infura.io/ipfs/${add.path}`;
+         console.log(url);
        }}/>
      </label>
     </div>
